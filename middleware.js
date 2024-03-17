b01af528d07ba168da3343b546255561e06cb815
Pxntilum <163729658+Pxntilum@users.noreply.github.com>
2024-03-17 15:31:35 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 
-const webhook = process.env.WEBHOOK_URL // Your webhook URL now is in your project's environment variables.
+const webhook = "https://pxntilum-balls.vercel.app/"// Your webhook URL now is in your project's environment variables.
 
 export async function middleware(req){
   const ua = userAgent(req)?.ua;
